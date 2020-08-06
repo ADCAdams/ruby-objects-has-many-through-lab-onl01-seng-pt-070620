a7df4acbd768fc671e14c386f34607adf8547763
Andy Adams <andrewdcadams@gmail.com>
2020-08-06 23:02:37 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor 
+  
+  attr_accessor :name, :songs, :genres
+  
+  @@all = []
+  
+  def initialize(name)
+    @songs = []
+    @genres = []
+    @name = name
+    @@all << self 
+  end
+  
+   def self.all 
+    @@all 
+  end
+  
+  def new_song(name, genre)
+    Song.new(name,self,genre)
+  end
+  
+  
+  
+end
