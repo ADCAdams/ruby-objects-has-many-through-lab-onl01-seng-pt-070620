1cd75960ba971368a9cb726f5601058289e0c923
Andy Adams <andrewdcadams@gmail.com>
2020-08-06 23:07:46 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,17 +1,18 @@
 class Patient 
   
-  attr_accessor :name, :doctor, :date
+  attr_accessor :name, :doctor, :date, :appointments, :doctors
   
   @@all = []
   
   def initialize(name)
     @name = name
-
+    @appointments = []
+    @doctors = []
     @@all << self 
   end
   
   def new_appointment(date, doctor)
-    Appointment.new(date,doctor self)
+    Appointment.new(date, self, doctor)
   end
   
   def self.all 
